f77d0d51728edbce77413fe6dad32c7c3cf6c760
yadhukrishanyadhukrishan <90526355+yadhukrishanyadhukrishan@users.noreply.github.com>
2022-04-18 20:50:52 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/yadhukrishanyadhukrishan/myran
+  git clone https://github.com/Aadhi000/Ajax.git /Ajax
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /myran
+  git clone $UPSTREAM_REPO /Ajax
 fi
-cd /myran
+cd /Ajax
 pip3 install -U -r requirements.txt
-echo "mrbean..🔥"
+echo "Starting ᗩᒍᗩ᙭....🔥"
 python3 bot.py
