d97bfa063ba70cdf306e56d27d4e6d72f0859770
yadhukrishanyadhukrishan <90526355+yadhukrishanyadhukrishan@users.noreply.github.com>
2022-09-30 20:27:53 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1334,10 +1334,7 @@ async def auto_filter(client, msg, spoll=False):
             **locals()
         )
     else:
-        cap = f"𝖭𝖺𝗆𝖾 : {title}
-𝖱𝖾𝗅𝖾𝖺𝗌𝖾 : {release_date}    
-𝖫𝖺𝗇𝗀𝗎𝖺𝗀𝖾𝗌 : {languages}
-𝖦𝖾𝗇𝗋𝖾 : {genres} {search}"
+        cap = f"𝖭𝖺𝗆𝖾 : {title}𝖱𝖾𝗅𝖾𝖺𝗌𝖾 : {release_date}    𝖫𝖺𝗇𝗀𝗎𝖺𝗀𝖾𝗌 :/n {languages}𝖦𝖾𝗇𝗋𝖾 : {genres} {search}"
     if imdb and imdb.get('poster'):
         try:
             await message.reply_photo(photo=imdb.get('poster'), caption=cap[:1024],
